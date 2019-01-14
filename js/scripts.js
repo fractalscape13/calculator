cb27a1377a6770bde2d16b7600dbc6f631ca723c
Garrett Rowley and Joseph Wangemann <garrett@>
2019-01-14 13:13:56 -0800

    created initial files for project

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(num1, num2){
+return num1 + num2
+};
+
+var num1 = parseInt(prompt("enter a number: "));
+var num2 = parseInt(prompt("enter another number: "));
+
+alert(add(num1, num2));
