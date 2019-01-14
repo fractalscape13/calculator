d5fc0547d26929242e77f10e91370dc6c14efafe
Garrett Rowley and Joseph Wangemann <garrett@>
2019-01-14 15:38:41 -0800

    created add, subtract, multiply and divide function in prompt style

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,32 @@
+var question = prompt("What would you like to do? Choose add, subtract, multiply or divide");
+var num1 = parseInt(prompt("enter a number: "));
+var num2 = parseInt(prompt("enter another number: "));
+
 var add = function(num1, num2){
 return num1 + num2
 };
 
-var num1 = parseInt(prompt("enter a number: "));
-var num2 = parseInt(prompt("enter another number: "));
+var subtract = function(num1, num2){
+return num1 - num2
+};
 
-alert(add(num1, num2));
+var multiply = function(num1, num2){
+return num1 * num2
+};
+
+var divide = function(num1, num2){
+return num1 / num2
+};
+
+
+if (question==="add") {
+  document.write("Your total is " + add(num1 ,num2));
+}  else if (question==="subtract") {
+  document.write("Your total is " + subtract(num1 ,num2));
+} else if (question==="multiply") {
+  document.write("Your total is " + multiply(num1 ,num2));
+} else if (question==="divide") {
+  document.write("Your total is " + divide(num1 ,num2));
+} else {
+  document.write("You did not enter a valid answer");
+}
